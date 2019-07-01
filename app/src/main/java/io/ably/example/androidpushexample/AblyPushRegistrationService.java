e86889b370afc1431ff360fea0b99ebfd393fb36
Amit S <amit@smargav.com>
2019-07-01 15:06:28 +0530

    Updated dependencies and support for androidx

--- app/src/main/java/io/ably/example/androidpushexample/AblyPushRegistrationService.java
+++
@@ -1,6 +0,0 @@
-package io.ably.example.androidpushexample;
-
-import io.ably.lib.push.AblyFirebaseInstanceIdService;
-
-public class AblyPushRegistrationService extends AblyFirebaseInstanceIdService {
-}
